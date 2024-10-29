44f2a50569c2d0282f5c8557092fd5b0cdb39813
Suha2305 <suhafatima.rr@gmail.com>
2024-10-29 19:51:23 +0400

    simplesums.py

---
+++ excercise 02/simplesums.py
@@ -0,0 +1,11 @@
+# Step 1: write first number(apple) as 8
+apple = 8
+
+# Step 2: write the second number(banana) as 10
+banana = 10
+
+# Step 3: write 
+mango = apple + banana
+
+# Step 4: Print the sum of apple and banana
+print("mango is", apple, "plus", banana, "which is:", mango)
