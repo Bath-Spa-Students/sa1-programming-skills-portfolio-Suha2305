63a51ac83568623fecff1e14599fda5e43114f9e
Suha2305 <suhafatima.rr@gmail.com>
2024-11-06 22:06:15 +0400

    simple_search.py

---
+++ excersice 08/simple_search.py
@@ -0,0 +1,11 @@
+# Type a list of strings containing names 
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+
+# write the string you want to search in tje list of strings mentioned above
+search_name = "Sam"
+
+# Search for the name in the list
+if search_name in names:
+    print("{search_name} was on the list.")
+else:
+    print("{search_name} was not on the list.")
