ca583c7ab5d9cd8d51393bb13376d053251e103d
Suha2305 <suhafatima.rr@gmail.com>
2024-11-12 18:45:32 +0400

    Update biography.py

--- excersice 03/biography.py
+++ excersice 03/biography.py
@@ -1,14 +1,14 @@
 # Step 1: Store the information in variables
-name = "Suha Fatima"         # String for name
-hometown = "Kilakarai"     # String for hometown
-age = 18                 # Integer for age
+name = "John Doe"         # String for name
+hometown = "New York"     # String for hometown
+age = 25                 # Integer for age
 
-# Step 2: Store the information in a dictionary
+# Step 2: Store the information in a dictionary using {}
 user_info = {
     "name": name,
     "hometown": hometown,
     "age": age
 }
 
-# Step 3: Print the values on separate lines using a single print() statement
-print(f"Name: {user_info['name']}\nHometown: {user_info['hometown']}\nAge: {user_info['age']}")
+# Step 3: Print the values on separate lines using \n
+print(f"Name: {user_info['name']}\nHometown: {user_info['hometown']}\nAge: {user_info['age']}") #using a single print() statement \n to print answers in different lines
