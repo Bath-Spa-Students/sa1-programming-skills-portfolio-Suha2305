e77c6a34b2cd50a3dc467908dd4d6584d7844b62
Suha2305 <suhafatima.rr@gmail.com>
2024-11-12 19:20:19 +0400

    Update days_of_the_month.py

--- excersice 05/days_of_the_month.py
+++ excersice 05/days_of_the_month.py
@@ -15,10 +15,12 @@ month_days = {
 }
 
 # Step 2: input the month number
-month_number = int(input("Please enter the month number (1-12): "))
+month_number = int(input("Please enter the month number (1-12): ")) # month_number Stores the entered month number (1-12). eg 3= march
+# the input function allows us to enter something
 
 # Step 3: Check if the input is valid and output the number of days in the month
-if 1 <= month_number <= 12:
+if 1 <= month_number <= 12:# this function  [ 1<= month_number <= 12:] Checks if the month number is between 1 and 12
     print(f"The number of days in month {month_number} is {month_days[month_number]}.")
 else:
-    print("Invalid month number! Please enter a number between 1 and 12.")
+    print("Invalid month number! Please enter a number between 1 and 12.")#if the month number entered is not between 1-12 this will be printed
+
