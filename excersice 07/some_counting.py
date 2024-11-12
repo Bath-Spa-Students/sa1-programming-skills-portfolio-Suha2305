4cd7b27ada051ad600552b67d5ed9477c196f9b9
Suha2305 <suhafatima.rr@gmail.com>
2024-11-12 20:22:45 +0400

    Update some_counting.py

--- excersice 07/some_counting.py
+++ excersice 07/some_counting.py
@@ -3,28 +3,28 @@ print("Counting up from 0 to 50 in increments of 1:")
 for i in range(0, 51, 1): # Generates numbers from 0, ending before 51, and increasing by 1.
     print(i) # Prints the current value of i
 
-print("\n" + "-"*40 + "\n")  # \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hypens then a blank line again which will help us seperate each set of out puts
+print("\n" + "-"*40 + "\n")  # \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hyphens then a blank line again which will help us separate each set of out puts
 
 # 2. Loop that counts down from 50 to 0 in decrements of 1
 print("Counting down from 50 to 0 in decrements of 1:")
 for i in range(50, -1, -1): # Generates numbers from 50 down to 0, decreasing by 1
     print(i) # Prints the current value of i
 
-print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hypens then a blank line again which will help us seperate each set of out puts 
+print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hyphens then a blank line again which will help us separate each set of out puts 
 
 # 3. Loop that counts up from 30 to 50 in increments of 1
 print("Counting up from 30 to 50 in increments of 1:")
 for i in range(30, 51, 1): #Generates numbers from 30 to 50, increasing by 1
     print(i) #Prints the current value of i
 
-print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hypens then a blank line again which will help us seperate each set of out puts 
+print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hyphens then a blank line again which will help us separate each set of out puts 
 
 # 4. Loop that counts down from 50 to 10 in decrements of 2
 print("Counting down from 50 to 10 in decrements of 2:")
 for i in range(50, 9, -2): # Generates numbers from 50 down to 10, decreasing by 2
     print(i) #Prints the current value of i
 
-print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hypens then a blank line again which will help us seperate each set of out puts 
+print("\n" + "-"*40 + "\n")# \n creates/adds a new line, "-"*40 Creates a line of 40 hyphens. this print function "print("\n" + "-"*40 + "\n")"m helps us to print a blank line then followed by 40 hyphens then a blank line again which will help us separate each set of out puts 
 
 # 5. Loop that counts up from 100 to 200 in increments of 5
 print("Counting up from 100 to 200 in increments of 5:")
