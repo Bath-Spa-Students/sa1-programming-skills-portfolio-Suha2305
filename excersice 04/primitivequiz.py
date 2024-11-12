3e9f31c34f3658bdea3899e9523f02ca455fda14
Suha2305 <suhafatima.rr@gmail.com>
2024-11-12 18:56:59 +0400

    Update primitivequiz.py

--- excersice 04/primitivequiz.py
+++ excersice 04/primitivequiz.py
@@ -2,7 +2,7 @@
 answer_france = input("What is the capital of France? ")
 
 # Step 2: Check if the answer is correct
-if answer_france.lower() == "paris":
-    print("Correct!")
+if answer_france.lower() == "paris": # by using the .lower() function the entire string/answer which is being inputted by the user will entirely be made into lower case. this is done to avoid case-sensitive mistakes during the quiz
+    print("Correct!")# if the user enters the answer paris/PARIS/Paris the answer will be still correct as we used the .lower() function
 else:
-    print("Wrong! The correct answer is Paris.")
+    print("Wrong! The correct answer is Paris.") # if the user enters any answer other than paris it will be deemed as wrong
