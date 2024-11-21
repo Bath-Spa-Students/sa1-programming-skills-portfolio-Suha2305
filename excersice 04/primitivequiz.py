f5f3e7b1459389db7c6699f3e9e087ae058beef0
Suha2305 <suhafatima.rr@gmail.com>
2024-11-21 14:23:31 +0400

    primitivequiz.py

--- excersice 04/primitivequiz.py
+++ excersice 04/primitivequiz.py
@@ -1,4 +1,4 @@
-# Step 1: what is the capital of France
+# Step 1: what is the capital of France  
 answer_france = input("What is the capital of France? ")
 
 # Step 2: Check if the answer is correct
